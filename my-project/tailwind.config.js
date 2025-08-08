53912966b7aba2ed4fabe97248618ca6addff457
FAEHD153 <f.3693ali@gmail.com>
2025-08-08 17:03:56 +0500

    Add files via upload

---
+++ my-project/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: "'Playfair Display', serif",
+        lato: "'Lato', sans-serif",
+      },
+       keyframes: {
+        'fade-up': {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-up': 'fade-up 0.8s ease-out forwards',
+      }
+    }
+  }
+    }
+
